f6305ef3dca5984194adac01b15cdc35c150e607
Troy Fitzgerald <troyfitzgerald3@gmail.com>
2023-02-20 15:32:42 -0500

    Neck changes

--- csc223Giraffes/Neck.java
+++ csc223Giraffes/Neck.java
@@ -1,5 +1,33 @@
 package csc223Giraffes;
 
+import java.util.Scanner;
+
 public class Neck {
 
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int minArrivalTime; 
+		int maxArrivalTime; 
+		int minServiceTime; 
+		int maxServiceTime; 
+		int numCustomers;
+
+		System.out.println("Enter minimum arrival time between customers:");
+		minArrivalTime = scan.nextInt();
+
+		System.out.println("Enter maximum arrival time between customers:");
+		maxArrivalTime = scan.nextInt();
+
+		System.out.println("Enter minimum service time:");
+		minServiceTime = scan.nextInt();
+
+		System.out.println("Enter maximum service time:");
+		maxServiceTime = scan.nextInt();
+
+		System.out.println("Enter number of customers to serve:");
+		numCustomers = scan.nextInt();
+		
+		
+	}
 }
